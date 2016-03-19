505efd4c6e7a5d4629ad3184bf72f066f37c68ab
Raj Abishek <rajabishek@hotmail.com>
2016-03-19 18:40:39 +0530

    Adds the new note view controller and establishes the segue

---
+++ Beyond/NotesDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NotesDetailViewController.swift
+//  Beyond
+//
+//  Created by Raj Abishek on 19/03/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import UIKit
+
+class NotesDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleTextView: UITextView!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
