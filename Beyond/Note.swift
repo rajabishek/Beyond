d15a39dff10ac71d97ee76f02aaa319704c82355
Raj Abishek <rajabishek@hotmail.com>
2016-03-19 16:49:39 +0530

    Adds the note model to manage the notes

---
+++ Beyond/Note.swift
@@ -0,0 +1,21 @@
+//
+//  Note.swift
+//  Beyond
+//
+//  Created by Raj Abishek on 19/03/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    
+    var title: String
+
+    var content: String
+    
+    init(title: String, content: String) {
+        self.title = title
+        self.content = content
+    }
+}
