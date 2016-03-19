6bf3c454011300186ddfba29e7f5dc78651b6485
Raj Abishek <rajabishek@hotmail.com>
2016-03-19 16:07:08 +0530

    Adds custom class for the note table view cell

---
+++ Beyond/NoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteTableViewCell.swift
+//  Beyond
+//
+//  Created by Raj Abishek on 19/03/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
