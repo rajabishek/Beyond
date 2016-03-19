0bc1c3b93798b51b2cc9e858aebd3978c1e6c9b4
Raj Abishek <rajabishek@hotmail.com>
2016-03-19 20:02:02 +0530

    Use the ui text view delegate to simulate the placeholder text for ui textview from the new note controller

--- Beyond/NewNoteViewController.swift
+++ Beyond/NewNoteViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class NewNoteViewController: UIViewController {
+class NewNoteViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    let contentViewPlaceHolderText = "Start Writing..."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Start Writing"
+        contentTextView.text = contentViewPlaceHolderText
+        contentTextView.textColor = UIColor.lightGrayColor()
+        contentTextView.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +30,37 @@ class NewNoteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textView.textColor == UIColor.lightGrayColor() {
+            textView.text = nil
+            textView.textColor = UIColor.blackColor()
+        }
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = contentViewPlaceHolderText
+            textView.textColor = UIColor.lightGrayColor()
+        }
+    }
+    
+    @IBAction func closeButtonWasPressed(sender: UIBarButtonItem) {
+        
+        if (titleTextField.text! == "" && contentTextView.text! == contentViewPlaceHolderText) || (titleTextField.text! == "" && contentTextView.text! == "") {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        } else {
+            let alertController = UIAlertController(title: "Caution", message: "You have something written. Sure don't wanna save ?", preferredStyle: .Alert)
+            
+            let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default, handler: nil)
+            let closeAction = UIAlertAction(title: "Never Mind", style: UIAlertActionStyle.Destructive, handler: { (action) -> Void in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            })
+            
+            alertController.addAction(saveAction)
+            alertController.addAction(closeAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
 
     /*
     // MARK: - Navigation
