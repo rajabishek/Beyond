5f0bbd26e1c237c4dbeb7db007dcd07bc1e1c342
Raj Abishek <rajabishek@hotmail.com>
2016-03-20 21:16:49 +0530

    Adds the method for logging in the auth manager

--- Beyond/AuthManager.swift
+++ Beyond/AuthManager.swift
@@ -24,11 +24,36 @@ class AuthManager {
     
     let baseUrl = "http://begin.dev/api/v1"
     
-    func loginWithEmail(email: String, password: String) {
+    func loginWithEmail(email: String, password: String, callback: (errorMessage: String?, token: String?) -> Void) {
         
         Alamofire.request(.POST, "\(baseUrl)/login", parameters: ["email": email, "password": password])
             .responseData { response in
-                print(response.result)
+                switch response.result {
+                case .Success:
+                    if let value = response.result.value {
+                        let json = JSON(value)
+                        print("\(json)")
+                        if let success = json["success"].bool {
+                            if success {
+                                if let token = json["data"]["token"].string {
+                                    callback(errorMessage: nil, token: token)
+                                }
+                            } else {
+                                for (_, errorMessage):(String, JSON) in json["errors"] {
+                                    if let message = errorMessage.string {
+                                        return callback(errorMessage: message, token: nil)
+                                    } else {
+                                        callback(errorMessage: "Please try after some time", token: nil)
+                                    }
+                                }
+                            }
+                        } else {
+                            callback(errorMessage: "Please try after some time", token: nil)
+                        }
+                    }
+                case .Failure:
+                    callback(errorMessage: "Please try after some time", token: nil)
+                }
         }
     }
     
