6b23b618975a864b9f4445469fcb595b3d190ebc
Raj Abishek <rajabishek@hotmail.com>
2016-03-20 22:21:35 +0530

    Adds the validation method for the email field

--- Beyond/Validator.swift
+++ Beyond/Validator.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+class Validator {
+    
+    func isValidEmail(testStr:String) -> Bool {
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluateWithObject(testStr)
+    }
+}
